2a08f50ca21d70fbd4221efeb111c368c92e843f
Anders Wind <awia00@gmail.com>
2014-02-03 16:27:42 +0100

    Kan nu opdele i vejnavn vejnummer+bogstav, sal, postnummer og bynavn.

--- addressparser/src/addressparser/Parser.java
+++ addressparser/src/addressparser/Parser.java
@@ -12,28 +12,33 @@ public class Parser {
     public String[] parseThis(String parseMe) {
 
         String[] result = new String[6];
-        String[] expressions = new String[6];
+        String[] expressions = new String[5];
         //debug
         System.out.println(parseMe);
         
-        expressions[0] = "^[\\sA-Øa-ø'-]{1,}";
-        expressions[1] = "";
-        expressions[2] = "[1-9]\\d{0,2}[\\-\\s]?[a-z]?";
-        expressions[3] = "(\\d{2}|\\d)[. -]*(sal| {2}|\\.)";
-        expressions[4] = "\\b[0-9]{4}\\b";
-        expressions[5] = ".*";
+        expressions[0] = "[\\sA-Øa-ø'-]{1,}";
+        expressions[1] = "[1-9]\\d{0,2}[\\-\\s]?[a-z]?";
+        expressions[2] = "(\\d{2}|\\d)[. -]*(sal| {2}|\\.)";
+        expressions[3] = "\\b[0-9]{4}\\b";
+        expressions[4] = "\\D*";
 
         for (int i = 0; i < expressions.length; i++) {
 
             Pattern pattern = Pattern.compile(expressions[i],Pattern.MULTILINE);
 
+            parseMe = parseMe.replace(" i ", " , ");
+            
             Matcher matcher = pattern.matcher(parseMe);
 
+            
             while (matcher.find()) {
                
                 String matchedString = parseMe.substring(matcher.start(), matcher.end());
 
-                result[i] = matchedString;
+                result[i] = matchedString.trim().replace(",", "");
+                
+                parseMe = parseMe.replace(matchedString, "");
+                break;
             }
 
         }
