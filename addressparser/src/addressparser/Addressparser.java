6f7bbb88488e46c1e20184ce8aaac4abcbe4da66
Anders Wind Steffensen <awia00@gmail.com>
2014-02-03 21:40:55 +0100

    Fixed road number and letter - and added some debugging print statements

--- addressparser/src/addressparser/Addressparser.java
+++ addressparser/src/addressparser/Addressparser.java
@@ -17,11 +17,19 @@ public class Addressparser {
         
         Parser ps = new Parser();
         
-        String[] test = ps.parseThis("Brøndby Nord Vej 234 4sal 2300 København S");
+        String[] test = ps.parseThis("Brøndby Nord Vej 234b 4.sal 2300 København S");
         
+        System.out.println("Printing the results");
         for(int i = 0; i < test.length ; i++){
             System.out.println(test[i]);
         }
+        System.out.println("Next test");
+        System.out.println("----------------------");
+        test = ps.parseThis("Brøndby Nord Vej 235 4.sal 2300 København S");
         
+        System.out.println("Printing the results");
+        for(int i = 0; i < test.length ; i++){
+            System.out.println(test[i]);
+        }
     }
 }
