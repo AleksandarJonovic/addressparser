927508352f80b4b3632b31a5273e6ce053b6fb5f
Anders Wind Steffensen <awia00@gmail.com>
2014-02-17 01:44:23 +0100

    Bare en anden streng som køres i programmet

--- src/addressparser/AddressMatcher.java
+++ src/addressparser/AddressMatcher.java
@@ -41,6 +41,6 @@ public class AddressMatcher {
      * @throws InvalidInputException
      */
     public static void main(String[] args) {
-        AddressMatcher am = new AddressMatcher("Brøndby Nord Vej");
+        AddressMatcher am = new AddressMatcher("Rued Langgaards Vej 9 københavn s");
     }
 }
